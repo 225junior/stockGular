55d4f22b0d91a542a498b5d8fbc6f528150cbdb8
225junior <225juniorbrindou@gmail.com>
2022-09-17 18:46:11 +0000

    repositionnement de composants statistiques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 import { PageDashboardComponent } from './page-dashboard/page-dashboard.component';
-import { PageStatistiquesComponent } from './page-statistiques/page-statistiques.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
 
 @NgModule({
   declarations: [
