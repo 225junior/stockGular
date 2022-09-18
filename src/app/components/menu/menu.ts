69cf4ef5ead525f174a3ecce9b759deaf6f291f1
225junior <225juniorbrindou@gmail.com>
2022-09-18 03:51:37 +0000

    creation du menu

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,7 +1,46 @@
-export interface menu{
-    id:string;
-    icon:string;
-    title:string;
-    url:string;
-    subMenu:menu[]
+export interface Menu {
+    id: number;
+    icon: string;
+    title: string;
+    url: string;
+    subMenu?: Menu[]
 }
+
+export const MenuData: Menu[] =
+    [
+        {
+            id: 1, icon: '', title: 'Tableau de Bord', url: '',
+            subMenu: [
+                { id: 11, icon: '', title: "Vue d'ensemble", url: '' },
+                { id: 12, icon: '', title: "Statistiques", url: '' }
+            ]
+        },
+        {
+            id: 2, icon: '', title: 'Articles', url: '',
+            subMenu: [
+                { id: 21, icon: '', title: "Articles", url: '' },
+                { id: 22, icon: '', title: "Mouvement stock", url: '' }
+            ]
+        },
+        {
+            id: 3, icon: '', title: 'Clients', url: '',
+            subMenu: [
+                { id: 31, icon: '', title: "Clients", url: '' },
+                { id: 32, icon: '', title: "Commandes Client", url: '' }
+            ]
+        },
+        {
+            id: 4, icon: '', title: 'Forunisseurs', url: '',
+            subMenu: [
+                { id: 41, icon: '', title: "Forunisseurs", url: '' },
+                { id: 42, icon: '', title: "Commandes Forunisseur", url: '' }
+            ]
+        },
+        {
+            id: 5, icon: '', title: 'Parametrages', url: '',
+            subMenu: [
+                { id: 51, icon: '', title: "Categories", url: '' },
+                { id: 52, icon: '', title: "Utilisateurs", url: '' }
+            ]
+        },
+    ]
