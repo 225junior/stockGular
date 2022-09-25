e41476540408a8b1c351688dc0e6743f8023de7b
225junior <225juniorbrindou@gmail.com>
2022-09-25 16:19:43 +0000

    boutton nouveau

--- src/app/pages/page-client/page-client/page-client.component.ts
+++ src/app/pages/page-client/page-client/page-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-client',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageClientComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  nouveauClient(): void {
+    this.router.navigate(['nouvel-article'])
   }
 
 }
