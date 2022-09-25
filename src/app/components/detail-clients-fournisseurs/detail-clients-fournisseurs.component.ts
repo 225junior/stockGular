25f43f9684aeba2e9ca56aaf96bf7c080ade179e
225junior <225juniorbrindou@gmail.com>
2022-09-25 13:59:56 +0000

    detail client forunisseur

---
+++ src/app/components/detail-clients-fournisseurs/detail-clients-fournisseurs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-clients-fournisseurs',
+  templateUrl: './detail-clients-fournisseurs.component.html',
+  styleUrls: ['./detail-clients-fournisseurs.component.scss']
+})
+export class DetailClientsFournisseursComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
