2a8f2104e307dffc06dc923f649c531b3a54654e
225junior <225juniorbrindou@gmail.com>
2022-10-28 06:13:39 +0000

    creation du gestionnaire de session

---
+++ src/app/api/core/app-storage-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+
+
+@Injectable()
+export class AppStorageServie {
+
+    getItem(key: string): string {
+        return window.localStorage[key]
+    }
+    setItem(key: string, value: string): void {
+        window.localStorage[key] = value
+    }
+    removeKey(key: string): void {
+        window.localStorage.removeItem(key)
+    }
+}
